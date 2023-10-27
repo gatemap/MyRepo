d72b4e153be443165300a9391aa4db928cd468ac
정주은 <spacernet@naver.com>
2023-10-27 16:47:11 +0900

    파일 입출력 실습 추가

--- PythonStudy_08.py
+++ PythonStudy_08.py
@@ -43,35 +43,69 @@ def practice_1() :
             
         members += 1
     '''
+    
     file = open("./memberInfo.txt", "r")
-    member_info = file.read()
-    print(file.read())
+    member_info = file.readlines()
+    # 사용자들 이름, 비밀번호를 한 줄로 저장
+    member_info = [i.strip() for i in member_info]
     file.close()
+    
+    print(member_info)
     
     # 이름, 비밀번호 입력받기
     input_name = input("이름을 입력하세요 : ")
     input_password = input("비밀번호를 입력하세요 : ")
     
-    # 사용자들 이름, 비밀번호를 한 줄로 리스트화
-    member_info_list = member_info.split('\n')
-    # 마지막 엔터가 리스트로 들어간 것 제거
-    member_info_list.pop()
-    member_info_detail = {}
-    
-    # 사용자 한 명에 대해서 이름과 비밀번호를 key : value로 저장
-    for data in member_info_list :
-        detail = data.split(', ')
-        member_info_detail[detail[0]] = detail[1]
-    
-    # key값과 value가 함께 동일한지 체크
+    # 로그인 성공/실패 체크
     login_success = False
-    for key in member_info_detail :
-        if input_name == key and input_password == member_info_detail[key] :
+    for i in member_info :
+        info = i.split(', ')
+        if info[0] == input_name and info[1] == input_password :
             login_success = True
             break
 
     print("로그인 성공" if login_success else "로그인 실패")
     
+    if login_success :
+        tel_num = input("전화번호를 입력하세요 : ")
+        
+        member_tel_path = Path("member_tel.txt")
+        
+        if member_tel_path.exists() :
+            file = open("./member_tel.txt", "r")
+            members_tel_info = file.readlines()
+            file.close()
+            
+            # 이미 존재하는 회원 데이터인지 조회
+            memberExist = False
+            for i in range(len(members_tel_info)) :
+                info = members_tel_info[i].split(', ')
+                if info[0] == input_name :
+                    memberExist = True
+                    # 수정된 정보로 변경(수정)
+                    members_tel_info[i] = f"{input_name}, {tel_num}\n"
+                    break
+            
+            # 사용자 이름이 이미 존재하는 경우(수정)
+            if memberExist :
+                with open("./member_tel.txt", "r+") as file :
+                    # 한 번 싹 날리고
+                    file.write('')
+                    file.seek(0, 2)
+                    # 갱신한 데이터 삽입
+                    for i in members_tel_info :
+                        file.write(i)
+                
+            # 신규 사용자의 경우
+            else :
+                with open("./member_tel.txt", "a") as file :
+                    file.write(f"{input_name}, {tel_num}\n")
+        
+        # 파일이 없는 경우 그냥 새로 기록
+        else :
+            with open("./member_tel.txt", "w") as file :
+                file.write(f"{input_name}, {tel_num}\n")
+            
 def main() :
     #fileInOut()
     practice_1()
