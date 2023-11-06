72399bc9f727ed8462f15cacabad5d8fdc288199
정주은 <spacernet@naver.com>
2023-11-06 17:05:22 +0900

    dash project 실습
    이제서야 commit함

---
+++ dash_study/dash_exmample.py
@@ -0,0 +1,198 @@
+'''
+from dash import Dash, html, dcc, Input, Output, callback
+import pandas as pd
+import plotly.express as px
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+
+df = pd.read_csv('https://plotly.github.io/datasets/country_indicators.csv')
+
+
+app.layout = html.Div([
+    html.Div([
+
+        html.Div([
+            dcc.Dropdown(
+                df['Indicator Name'].unique(),
+                'Fertility rate, total (births per woman)',
+                id='crossfilter-xaxis-column',
+            ),
+            dcc.RadioItems(
+                ['Linear', 'Log'],
+                'Linear',
+                id='crossfilter-xaxis-type',
+                labelStyle={'display': 'inline-block', 'marginTop': '5px'}
+            )
+        ],
+        style={'width': '49%', 'display': 'inline-block'}),
+
+        html.Div([
+            dcc.Dropdown(
+                df['Indicator Name'].unique(),
+                'Life expectancy at birth, total (years)',
+                id='crossfilter-yaxis-column'
+            ),
+            dcc.RadioItems(
+                ['Linear', 'Log'],
+                'Linear',
+                id='crossfilter-yaxis-type',
+                labelStyle={'display': 'inline-block', 'marginTop': '5px'}
+            )
+        ], style={'width': '49%', 'float': 'right', 'display': 'inline-block'})
+    ], style={
+        'padding': '10px 5px'
+    }),
+
+    html.Div([
+        dcc.Graph(
+            id='crossfilter-indicator-scatter',
+            hoverData={'points': [{'customdata': 'Japan'}]}
+        )
+    ], style={'width': '49%', 'display': 'inline-block', 'padding': '0 20'}),
+    html.Div([
+        dcc.Graph(id='x-time-series'),
+        dcc.Graph(id='y-time-series'),
+    ], style={'display': 'inline-block', 'width': '49%'}),
+
+    html.Div(dcc.Slider(
+        df['Year'].min(),
+        df['Year'].max(),
+        step=None,
+        id='crossfilter-year--slider',
+        value=df['Year'].max(),
+        marks={str(year): str(year) for year in df['Year'].unique()}
+    ), style={'width': '49%', 'padding': '0px 20px 20px 20px'})
+])
+
+
+@callback(
+    Output('crossfilter-indicator-scatter', 'figure'),
+    Input('crossfilter-xaxis-column', 'value'),
+    Input('crossfilter-yaxis-column', 'value'),
+    Input('crossfilter-xaxis-type', 'value'),
+    Input('crossfilter-yaxis-type', 'value'),
+    Input('crossfilter-year--slider', 'value'))
+def update_graph(xaxis_column_name, yaxis_column_name,
+                 xaxis_type, yaxis_type,
+                 year_value):
+    dff = df[df['Year'] == year_value]
+
+    fig = px.scatter(x=dff[dff['Indicator Name'] == xaxis_column_name]['Value'],
+            y=dff[dff['Indicator Name'] == yaxis_column_name]['Value'],
+            hover_name=dff[dff['Indicator Name'] == yaxis_column_name]['Country Name']
+            )
+
+    fig.update_traces(customdata=dff[dff['Indicator Name'] == yaxis_column_name]['Country Name'])
+
+    fig.update_xaxes(title=xaxis_column_name, type='linear' if xaxis_type == 'Linear' else 'log')
+
+    fig.update_yaxes(title=yaxis_column_name, type='linear' if yaxis_type == 'Linear' else 'log')
+
+    fig.update_layout(margin={'l': 40, 'b': 40, 't': 10, 'r': 0}, hovermode='closest')
+
+    return fig
+
+
+def create_time_series(dff, axis_type, title):
+
+    fig = px.scatter(dff, x='Year', y='Value')
+
+    fig.update_traces(mode='lines+markers')
+
+    fig.update_xaxes(showgrid=False)
+
+    fig.update_yaxes(type='linear' if axis_type == 'Linear' else 'log')
+
+    fig.add_annotation(x=0, y=0.85, xanchor='left', yanchor='bottom',
+                       xref='paper', yref='paper', showarrow=False, align='left',
+                       text=title)
+
+    fig.update_layout(height=225, margin={'l': 20, 'b': 30, 'r': 10, 't': 10})
+
+    return fig
+
+
+@callback(
+    Output('x-time-series', 'figure'),
+    Input('crossfilter-indicator-scatter', 'hoverData'),
+    Input('crossfilter-xaxis-column', 'value'),
+    Input('crossfilter-xaxis-type', 'value'))
+def update_x_timeseries(hoverData, xaxis_column_name, axis_type):
+    country_name = hoverData['points'][0]['customdata']
+    dff = df[df['Country Name'] == country_name]
+    dff = dff[dff['Indicator Name'] == xaxis_column_name]
+    title = '<b>{}</b><br>{}'.format(country_name, xaxis_column_name)
+    return create_time_series(dff, axis_type, title)
+
+
+@callback(
+    Output('y-time-series', 'figure'),
+    Input('crossfilter-indicator-scatter', 'hoverData'),
+    Input('crossfilter-yaxis-column', 'value'),
+    Input('crossfilter-yaxis-type', 'value'))
+def update_y_timeseries(hoverData, yaxis_column_name, axis_type):
+    dff = df[df['Country Name'] == hoverData['points'][0]['customdata']]
+    dff = dff[dff['Indicator Name'] == yaxis_column_name]
+    return create_time_series(dff, axis_type, yaxis_column_name)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+'''
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.graph_objs as go
+import random
+
+# Dash 애플리케이션 초기화
+app = dash.Dash(__name__)
+
+# 초기 데이터 생성
+x_data = list(range(10))
+y1_data = [random.randint(1, 10) for _ in range(10)]
+y2_data = [random.randint(10, 20) for _ in range(10)]
+
+# 그래프 생성
+app.layout = html.Div([
+    dcc.Graph(
+        id='dual-y-axis-graph',
+        config={'displayModeBar': False}
+    ),
+    dcc.Interval(
+        id='interval-component',
+        interval=5000,
+        n_intervals=0
+    )
+])
+
+# 그래프 업데이트 콜백 함수
+@app.callback(Output('dual-y-axis-graph', 'figure'), Input('interval-component', 'n_intervals'))
+def update_graph(n):
+    # 무작위 데이터 생성 (업데이트용)
+    y1_data = [random.randint(1, 10) for _ in range(10)]
+    y2_data = [random.randint(10, 20) for _ in range(10)]
+
+    # 그래프 업데이트
+    figure = go.Figure()
+
+    # Y1 데이터 추가
+    figure.add_trace(go.Scatter(x=x_data, y=y1_data, name='Y1', yaxis='y1'))
+
+    # Y2 데이터 추가
+    figure.add_trace(go.Scatter(x=x_data, y=y2_data, name='Y2', yaxis='y2'))
+
+    # 축 설정
+    figure.update_layout(
+        title='이중 Y 축 그래프',
+        yaxis=dict(title='Y1', side='left', showgrid=False),
+        yaxis2=dict(title='Y2', overlaying='y', side='right', showgrid=False),
+    )
+
+    return figure
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
--- dash_study/dash_project.py
+++ dash_study/dash_project.py
@@ -1,24 +1,70 @@
+import dash
 from dash import Dash, html, dcc, callback, Output, Input, dash_table
 import plotly.express as px
 import pandas as pd
 
 def main() :
     
-    dt = pd.read_csv('C:/Users/space/Desktop/MyRepo/dash_study/서울시 코로나19 확진자 발생동향 (2023.05.31.이전).csv', encoding='EUC-KR')
-    dt['서울시 기준일'].sort_values()
+    df = pd.read_csv('C:/Users/space/Desktop/MyRepo/dash_study/서울시 코로나19 확진자 발생동향 (2023.05.31.이전).csv', encoding='EUC-KR')
+    
+    # 역순으로 만들기
+    df = df[::-1]
     
     app = Dash(__name__)
     
     app.layout = html.Div([
     html.H1(children='서울시 코로나19 확진자 발생동향', style={'textAlign' : 'center'}),
-    #dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
-    #dcc.Graph(id='graph-content')
-    #dash_table.DataTable(dt.to_dict('records'))
-    dash_table.DataTable(dt.to_dict('records'),[{"name": i, "id": i} for i in dt.columns], id='tbl')
+    dcc.Graph(id='graph-content'),
+    dcc.Interval(
+        id='interval-component',
+        interval=1000,
+        n_intervals=0
+    )
 ])
-    
+   
+    @callback(
+        Output('graph-content', 'figure'),
+        Input('interval-component', 'n_intervals')
+    )
+    def update_graph(s) : 
+        
+        #start_idx = s * 20
+        end_idx = (s + 1) * 20
+        data_plot = df[:end_idx]
+        
+        figure = {
+            'data' : [
+                {'x' : data_plot['서울시 기준일'], 'y' : data_plot['서울시 사망'] / data_plot['서울시 확진자'] * 100, 'type' : 'line'}],
+            'layout' : {
+                'xaxis' : {'title' : '서울시 기준일'},
+                'yaxis' : {'title' : '서울시 사망률(%)'}
+            }
+        }
+        
+        # 마지막에 도달하면 더이상 update하지 않음
+        if data_plot.index[-1] == df.index[-1] :
+            raise dash.exceptions.PreventUpdate
+        
+        return figure
     
     app.run(debug=True)
+    
+    
+    
+def drawPerDeathGraph(df, fig, s) :
+    # 역순으로 만들기
+    df = df[::-1]
+    
+    end_idx = (s + 1) * 20
+    data_plot = df[:end_idx]
+    
+    fig.append_trace({
+        'x':data_plot['서울시 기준일'],
+        'y':data_plot['서울시 사망'] / data_plot['서울시 확진자'] * 100,
+        'name' : '서울시 확진 대비 사망률',
+        'mode' : 'line'
+    }, 1, 1)
+    
     
 if __name__ == '__main__' :
     main()
