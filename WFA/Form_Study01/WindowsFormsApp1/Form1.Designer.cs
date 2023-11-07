5445396f5f23dbf25ad313ee96d8bf7d79a2f17b
정주은 <spacernet@naver.com>
2023-11-07 19:03:26 +0900

    form modify
    계산기 완성하다가 다 못했음

--- WFA/Form_Study01/WindowsFormsApp1/Form1.Designer.cs
+++ WFA/Form_Study01/WindowsFormsApp1/Form1.Designer.cs
@@ -29,26 +29,96 @@
         private void InitializeComponent()
         {
             this.textBox_print = new System.Windows.Forms.TextBox();
+            this.numBox2 = new System.Windows.Forms.TextBox();
+            this.numBox1 = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.subButton = new System.Windows.Forms.Button();
+            this.multiButton = new System.Windows.Forms.Button();
+            this.divideButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox_print
             // 
-            this.textBox_print.Location = new System.Drawing.Point(30, 22);
+            this.textBox_print.Location = new System.Drawing.Point(34, 28);
+            this.textBox_print.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox_print.Multiline = true;
             this.textBox_print.Name = "textBox_print";
-            this.textBox_print.Size = new System.Drawing.Size(281, 182);
+            this.textBox_print.Size = new System.Drawing.Size(321, 226);
             this.textBox_print.TabIndex = 0;
             this.textBox_print.Text = " ";
             this.textBox_print.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
+            // numBox2
+            // 
+            this.numBox2.Location = new System.Drawing.Point(201, 272);
+            this.numBox2.Name = "numBox2";
+            this.numBox2.Size = new System.Drawing.Size(154, 25);
+            this.numBox2.TabIndex = 1;
+            this.numBox2.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // numBox1
+            // 
+            this.numBox1.Location = new System.Drawing.Point(34, 272);
+            this.numBox1.Name = "numBox1";
+            this.numBox1.Size = new System.Drawing.Size(151, 25);
+            this.numBox1.TabIndex = 3;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(34, 303);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 4;
+            this.addButton.Text = "+";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // subButton
+            // 
+            this.subButton.Location = new System.Drawing.Point(115, 303);
+            this.subButton.Name = "subButton";
+            this.subButton.Size = new System.Drawing.Size(75, 23);
+            this.subButton.TabIndex = 5;
+            this.subButton.Text = "-";
+            this.subButton.UseVisualStyleBackColor = true;
+            this.subButton.Click += new System.EventHandler(this.subButton_Click);
+            // 
+            // multiButton
+            // 
+            this.multiButton.Location = new System.Drawing.Point(201, 303);
+            this.multiButton.Name = "multiButton";
+            this.multiButton.Size = new System.Drawing.Size(75, 23);
+            this.multiButton.TabIndex = 6;
+            this.multiButton.Text = "*";
+            this.multiButton.UseVisualStyleBackColor = true;
+            this.multiButton.Click += new System.EventHandler(this.multiButton_Click);
+            // 
+            // divideButton
+            // 
+            this.divideButton.Location = new System.Drawing.Point(282, 303);
+            this.divideButton.Name = "divideButton";
+            this.divideButton.Size = new System.Drawing.Size(75, 23);
+            this.divideButton.TabIndex = 7;
+            this.divideButton.Text = "/";
+            this.divideButton.UseVisualStyleBackColor = true;
+            this.divideButton.Click += new System.EventHandler(this.divideButton_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(914, 562);
+            this.Controls.Add(this.divideButton);
+            this.Controls.Add(this.multiButton);
+            this.Controls.Add(this.subButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.numBox1);
+            this.Controls.Add(this.numBox2);
             this.Controls.Add(this.textBox_print);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,6 +127,12 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox_print;
+        private System.Windows.Forms.TextBox numBox2;
+        private System.Windows.Forms.TextBox numBox1;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button subButton;
+        private System.Windows.Forms.Button multiButton;
+        private System.Windows.Forms.Button divideButton;
     }
 }
 
--- WFA/Form_Study01/WindowsFormsApp1/Form1.cs
+++ WFA/Form_Study01/WindowsFormsApp1/Form1.cs
@@ -12,16 +12,119 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        List<int> numList = new List<int>();
         public Form1()
         {
             InitializeComponent();
 
             //MessageBox.Show("Hellow, world!");
 
-            textBox_print.Text = "멀티\r\n라인\r\n텍스트박스\r\n입니다.";
+            //textBox_print.Text = "멀티\r\n라인\r\n텍스트박스\r\n입니다.";
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = 0;
+
+            if (int.TryParse(numBox2.Text, out n))
+                textBox_print.Text = (n + 100).ToString();
+            else
+                MessageBox.Show("숫자를 입력하세요.");
+
+            //textBox_print.Text = textInputBox.Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        void Calculation(string fom)
+        {
+            int num = 0;
+
+            if(int.TryParse(fom, out num))
+            {
+                textBox_print.Text = num.ToString() + "\r\n";
+                numList.Add(num);
+            }
+            else
+            {
+                // 숫자보다 연산자가 먼저 들어온 경우
+                if(numList.Count < 1)
+                {
+                    MessageBox.Show("Error! 입력된 숫자가 없습니다");
+                    return;
+                }
+
+                switch (fom)
+                {
+                    case "+":
+                        textBox_print.Text += fom + "\r\n";
+                        break;
+                    case "-":
+                        textBox_print.Text += fom + "\r\n";
+                        break;
+                    case "*":
+                        textBox_print.Text += fom + "\r\n";
+                        break;
+                    case "/":
+                        textBox_print.Text += fom + "\r\n";
+                        break;
+                    case "=":
+
+                        break;
+                    default:
+                        MessageBox.Show("Error! 연산자 에러");
+                        break;
+                }
+            }
+
+
+            if(fom.Contains('+') || fom.Contains('-') || fom.Contains('*') || fom.Contains('/'))
+            {
+                string oper = fom.Contains('+') ? "+" : fom.Contains('-') ? "-" : fom.Contains('*') ? "*" : fom.Contains('/') ? "/" : string.Empty;
+                
+                // 사칙연산 이외의 연산자는 반환 처리
+                if(string.IsNullOrEmpty(oper))
+                {
+                    
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error! 연산자가 입력되지 않았습니다.");
+                numBox2.Text = string.Empty;
+            }    
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void subButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void multiButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void divideButton_Click(object sender, EventArgs e)
         {
 
         }
