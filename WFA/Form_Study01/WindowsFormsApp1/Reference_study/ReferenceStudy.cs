f63724d38af7f1560e2c25e8056e8995f8720716
정주은 <spacernet@naver.com>
2023-11-13 17:49:25 +0900

    참조 실습 추가

---
+++ WFA/Form_Study01/WindowsFormsApp1/Reference_study/ReferenceStudy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Reference_study
+{
+    internal class ReferenceStudy
+    {
+        int[] num1;
+        int[] num2;
+
+        public ReferenceStudy() 
+        {
+            num1 = new int[10];
+            FillArray(ref num1);
+            FillOutArray(out num2);
+        }
+
+        void FillArray(ref int[] array) 
+        {
+            for (int i = 0; i < array.Length; i++)
+                array[i] = i + 1;
+        }
+
+        void FillOutArray(out int[] array)
+        {
+            array = new int[10];
+
+            FillArray(ref array);
+        }
+    }
+}
