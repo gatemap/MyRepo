7c8ca35367fd7ac3e57c550e7f932f2383b2f3a1
정주은 <spacernet@naver.com>
2024-01-04 19:29:55 +0900

    delegate, event 연습

---
+++ WFA/Form_Study04/DelegateEventPractice/Test.cs
@@ -0,0 +1,17 @@
+﻿
+namespace DelegateEventPractice
+{
+    class Test
+    {
+        int m_nCurrentIndex = 0;
+        public delegate void IndexChangedEventHandler();
+        public event IndexChangedEventHandler ChangeCurrentIndex;
+
+        public void ChangePage(int nIndex)
+        {
+            m_nCurrentIndex = nIndex;
+            ChangeCurrentIndex();
+            m_nCurrentIndex = m_nCurrentIndex + 1;
+        }
+    }
+}
