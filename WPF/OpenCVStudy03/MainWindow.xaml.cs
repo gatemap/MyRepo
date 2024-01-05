21c6842db060f01bf7e264d761a79d4b3711f1f0
정주은 <spacernet@naver.com>
2024-01-05 17:24:40 +0900

    필터 수업

---
+++ WPF/OpenCVStudy03/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using OpenCvSharp;
+
+namespace OpenCVStudy03
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : System.Windows.Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
