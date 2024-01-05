a5e1a451cdae7387844c72c60d579163a0c547ad
정주은 <spacernet@naver.com>
2024-01-05 17:24:07 +0900

    카메라 실습2

--- WPF/OpenCVStudy02/OpenCVStudy02/MainWindow.xaml.cs
+++ WPF/OpenCVStudy02/OpenCVStudy02/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using OpenCvSharp;
-using System.Diagnostics;
 
 namespace OpenCVStudy02
 {
@@ -8,10 +7,11 @@ namespace OpenCVStudy02
     /// </summary>
     public partial class MainWindow : System.Windows.Window
     {
-        Mat src;
+        Mat src, frame;
 
         readonly string cvWindow = "White board";
         readonly string videoFileName = "coding_on.mp4";
+        readonly string camView = "Camera view";
 
         public MainWindow()
         {
@@ -19,7 +19,11 @@ namespace OpenCVStudy02
 
             //DrawBoard();
             //PlayVideo();
-            CamCapture();
+
+            frame = new Mat();
+            
+            //CamCapture();
+            CamCapturePractice();
         }
 
         #region 그리기
@@ -53,8 +57,7 @@ namespace OpenCVStudy02
         void PlayVideo()
         {
             VideoCapture capture = new VideoCapture(videoFileName);
-            Mat frame = new Mat();
-
+            
             while(true)
             {
                 if (capture.PosFrames == capture.FrameCount) 
@@ -75,8 +78,7 @@ namespace OpenCVStudy02
         void CamCapture()
         {
             VideoCapture capture = new VideoCapture(0);
-            Mat frame = new Mat();
-
+            
             capture.Set(VideoCaptureProperties.FrameWidth, 640);
             capture.Set(VideoCaptureProperties.FrameHeight, 480);
 
@@ -94,6 +96,44 @@ namespace OpenCVStudy02
 
             capture.Release();
             Cv2.DestroyAllWindows();
+        }
+
+        void CamCapturePractice()
+        {
+            VideoCapture capture = new VideoCapture(0);
+
+            capture.Set(VideoCaptureProperties.FrameWidth, 640);
+            capture.Set(VideoCaptureProperties.FrameHeight, 480);
+
+            capture.Read(frame);
+            Cv2.ImShow(camView, frame);
+
+            MouseCallback cvMouseCallback = new MouseCallback(CursurLocDraw);
+            Cv2.SetMouseCallback(camView, cvMouseCallback, frame.CvPtr);
+
+            while (true)
+            {
+                if (capture.IsOpened())
+                {
+                    capture.Read(frame);
+                    Cv2.ImShow(camView, frame);
+
+                    if (Cv2.WaitKey(33) == 'q')
+                        break;
+                }
+            }
+
+            capture.Release();
+            Cv2.DestroyAllWindows();
+        }
+
+        void CursurLocDraw(MouseEventTypes @event, int x, int y, MouseEventFlags flags, IntPtr userdate)
+        {
+            if (flags == MouseEventFlags.LButton)
+            {
+                Cv2.Circle(frame, new Point(x, y), 5, Scalar.Red, -1);
+                Cv2.ImShow(camView, frame);
+            }
         }
     }
 }
